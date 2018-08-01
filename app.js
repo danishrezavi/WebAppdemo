8c0c11121bae643ec4fcab5a64de05129b5929a9
Geeksread <geeksread@hotmail.com>
2018-08-01 19:33:45 +0500

    Adding Mongoose module and description input

--- app.js
+++ app.js
@@ -1,41 +1,64 @@
 var express =require("express");
 var app =express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelpcampdb"); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-	{name: "Babusar Top", image: "http://www.royalhotel.pk/wp-content/uploads/2013/08/babusar-top-815x338.jpg"},
-	{name: "Kashmir", image: "https://www.photosforclass.com/download/flickr-6333668591"},
-	{name: "Chitral", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtW3N7KpQJs0z9dmJvMXBoLrfzjFts1T9DsFfeT7ghFl1qwIga-A"},
-	{name: "Kashmir", image: "https://www.photosforclass.com/download/flickr-6333668591"},
-	{name: "Chitral", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtW3N7KpQJs0z9dmJvMXBoLrfzjFts1T9DsFfeT7ghFl1qwIga-A"},
-	{name: "Kashmir", image: "https://www.photosforclass.com/download/flickr-6333668591"},
-	{name: "Chitral", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtW3N7KpQJs0z9dmJvMXBoLrfzjFts1T9DsFfeT7ghFl1qwIga-A"}
-	];
+var campgroundSchema =new mongoose.Schema({
+	name: String ,
+	image: String,
+	description: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds:allCampgrounds});
+		}
+	});
 
-
-	res.render("campgrounds",{campgrounds:campgrounds});
+	//res.render("campgrounds",{campgrounds:campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
 	//res.send("you hit the post route!")
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image}
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds")
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc}
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
 });
 
 app.get("/campgrounds/new", function(req, res){
 	res.render("new.ejs");
+})
+
+app.get("/campgrounds/:id", function(req, res){
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else{
+			res.render("show", {campground: foundCampground});
+		}
+	});
+	req.params.id
+	
 })
 
 app.listen(3000, function() { 
